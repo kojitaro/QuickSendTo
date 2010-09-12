4c401914269e3662cb62041b51f57fbece3ef284
kojitaro <kohura12345678@gmail.com>
2010-09-12 11:12:06 +0900

    update for version1.2

--- sign.sh
+++ sign.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 ant clean
 ant release
 jarsigner -keystore  android.keystore  -verbose  bin/QuickSendTo-unsigned.apk kouji
+zipalign -v 4 bin/QuickSendTo-unsigned.apk QuickSendTo.apk
